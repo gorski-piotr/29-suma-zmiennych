6a98afdffe475ea512b39b0a3b71112793ddaaea
Piotr Gorski <gorskipiotr5@gmail.com>
2021-01-24 23:07:21 +0000

    my first function

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 13;
+let secondNumber = 7;
+
+function suma (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+console.log('suma = ' + suma(firstNumber, secondNumber));
